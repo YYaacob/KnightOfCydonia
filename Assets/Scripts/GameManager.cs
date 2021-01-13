39a6a9249f37932cd5297d2b6a387b65de1b43c8
Yaacob <150936@student.horizoncollege.nl>
2021-01-13 20:49:48 +0100

    game basics done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,15 +7,21 @@ public class GameManager : MonoBehaviour
 {
     public GameObject homeMenu;
     public GameObject gameOverMenu;
+    public GameObject congratsMenu;
 
     public float speed = 2;
     public Renderer bg;
     public GameObject col;
     public GameObject rock1;
+    public GameObject enemy1;
+    public GameObject enemy2;
     public GameObject rock2;
+    public GameObject queen;
     public bool gameOver = false;
+    public bool congrats = false;
     public List<GameObject> cols;
-    public List<GameObject> obstacles;
+    public List<GameObject> enemies;
+    public List<GameObject> queens;
     public bool start = false;
 
     // Start is called before the first frame update
@@ -27,9 +33,12 @@ public class GameManager : MonoBehaviour
             cols.Add(Instantiate(col, new Vector2(-10 + i, -3), Quaternion.identity));
         }
 
-        // Create Obstacles
-        obstacles.Add(Instantiate(rock1, new Vector2(14, -2), Quaternion.identity));
-        obstacles.Add(Instantiate(rock2, new Vector2(18, -2), Quaternion.identity));
+        float rPosition = Random.Range(20, 100);
+        queens.Add(Instantiate(queen, new Vector2(rPosition, -2), Quaternion.identity));
+
+        // Create enemies
+        enemies.Add(Instantiate(enemy1, new Vector2(14, -2.2f), Quaternion.identity));
+        enemies.Add(Instantiate(enemy2, new Vector2(18, -2.2f), Quaternion.identity));
     }
 
     // Update is called once per frame
@@ -42,9 +51,19 @@ public class GameManager : MonoBehaviour
                 start = true;
             }
         }
-        if (start == true && gameOver == true)
+        if (start == true && gameOver == true && congrats == false)
         {
             gameOverMenu.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+
+        if (start == true && congrats == true && gameOver == false)
+        {
+            congratsMenu.SetActive(true);
+            gameOverMenu.SetActive(false);
             if (Input.GetKeyDown(KeyCode.X))
             {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
@@ -68,14 +87,24 @@ public class GameManager : MonoBehaviour
 
             // Move obstacles
 
-            for (int i = 0; i < obstacles.Count; i++)
+            for (int i = 0; i < enemies.Count; i++)
             {
-                if (obstacles[i].transform.position.x <= -10)
+                if (enemies[i].transform.position.x <= -10)
                 {
                     float rObstacle = Random.Range(11, 18);
-                    obstacles[i].transform.position = new Vector3(rObstacle, -2, 0);
+                    enemies[i].transform.position = new Vector3(rObstacle, -2.2f, 0);
                 }
-                obstacles[i].transform.position = obstacles[i].transform.position + new Vector3(-1, 0, 0) * Time.deltaTime * speed;
+                enemies[i].transform.position = enemies[i].transform.position + new Vector3(-1, 0, 0) * Time.deltaTime * speed;
+            }
+
+            for (int i = 0; i < queens.Count; i++)
+            {
+                if (queens[i].transform.position.x <= -10)
+                {
+                    float rObstacle = Random.Range(11, 18);
+                    queens[i].transform.position = new Vector3(rObstacle, -2.2f, 0);
+                }
+                queens[i].transform.position = queens[i].transform.position + new Vector3(-1, 0, 0) * Time.deltaTime * speed;
             }
         }
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     public float jumpingHeight;
     private Rigidbody2D rigidbody2D;
     private Animator animator;
+    public float speed = 2f;
 
     public GameManager gameManager;
 
@@ -24,19 +25,33 @@ public class Player : MonoBehaviour
         {
             animator.SetBool("isJumping", true);
             rigidbody2D.AddForce(new Vector2(0, jumpingHeight));
+        }
 
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody2D.velocity = new Vector2(speed * -1, rigidbody2D.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Ground")
+        if (collision.gameObject.tag == "Ground")
         {
             animator.SetBool("isJumping", false);
-
         }
-        if(collision.gameObject.tag == "Obstacle")
+
+        if (collision.gameObject.tag == "Enemy")
         {
             gameManager.gameOver = true;
+        }
+
+        if (collision.gameObject.tag == "Queen")
+        {
+            gameManager.congrats = true;
         }
     }
 }
